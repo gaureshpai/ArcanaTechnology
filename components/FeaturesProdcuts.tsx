b81cf92a913bf17059c5eeb9a100234d6232241a
paigauresh <paigauresh@gmail.com>
2024-08-01 01:00:59 +0530

    featuredproducts styling changes

--- components/FeaturesProdcuts.tsx
+++ components/FeaturesProdcuts.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const FeaturedProducts = () => {
     return (
-        <div className="bg-white">
+        <div className="bg-white mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
                 <h2 className="text-3xl font-extrabold text-gray-900">Our Products</h2>
                 <div className="mt-12 grid gap-16 lg:grid-cols-2 lg:gap-x-8 lg:gap-y-12">
@@ -11,7 +11,7 @@ const FeaturedProducts = () => {
                         <p className="mt-3 text-base text-gray-500">
                             A smart IOT device for enhanced border security surveillance.
                         </p>
-                        <Link href="/products/bss" className="mt-4 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                        <Link href="/#bss" className="mt-4 text-sm font-medium text-indigo-600 hover:text-indigo-500">
                             Learn more <span aria-hidden="true">&rarr;</span>
                         </Link>
                     </div>
@@ -20,7 +20,7 @@ const FeaturedProducts = () => {
                         <p className="mt-3 text-base text-gray-500">
                             Multi-functional security solution for various applications.
                         </p>
-                        <Link href="/products/ssp" className="mt-4 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                        <Link href="/#ssp" className="mt-4 text-sm font-medium text-indigo-600 hover:text-indigo-500">
                             Learn more <span aria-hidden="true">&rarr;</span>
                         </Link>
                     </div>
