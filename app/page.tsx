a339ce4184c498b1a51e8a70103b49534d18e0b5
paigauresh <paigauresh@gmail.com>
2024-08-01 01:00:14 +0530

    Homepage updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+import About from '@/components/About';
+import Bss from '@/components/bss';
 import Hero from '@/components/Hero';
 import ProductSection from '@/components/ProductSection';
+import Ssp from '@/components/ssp';
+import Section from '@/components/Section';
 
 const Home = () => {
   return (
     <div>
       <Hero />
-      <ProductSection />
+      <Section number="1" title="Products">
+        <ProductSection />
+      </Section>
+      <Section number="2" title="About Us">
+        <About />
+      </Section>
+      <Section number="3" title="BSS">
+        <Bss />
+      </Section>
+      <Section number="4" title="SSP">
+        <Ssp />
+      </Section>
     </div>
   );
 };
