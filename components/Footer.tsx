c7f47f1e43f0a2636edd9255f68d8adc894ab125
paigauresh <paigauresh@gmail.com>
2024-08-02 23:55:43 +0530

    logo changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import logo from '@/public/images/logo.png';
+import logo from '@/public/images/footer.png';
 import { FaEnvelope, FaPhone, FaMapMarkerAlt } from 'react-icons/fa';
 import '@/styles/globals.css';
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-900 text-white pt-8 pb-4">
+    <footer className="bg-slate-900 text-white pt-8 pb-1">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="flex flex-col items-center">
@@ -16,17 +16,16 @@ const Footer = () => {
                 <Image
                   src={logo}
                   alt="BSS Logo"
-                  className="max-h-[12vh] w-auto"
-                  width={0}
-                  height={0}
+                  className="w-auto"
+                  width={150}
+                  height={150}
                   priority
                 />
               </Link>
             </div>
-            <p className="text-sm text-gray-300 text-center">&copy; {new Date().getFullYear()} Border Security System.</p>
           </div>
           <div>
-            <h2 className="text-xl font-bold mb-4">Contact Us</h2>
+            <h2 className="text-xl font-bold mb-1">Contact Us</h2>
             <ul className="space-y-2">
               <li className="flex items-center">
                 <FaEnvelope className="mr-2" />
@@ -41,6 +40,9 @@ const Footer = () => {
                 <span>Bantwal, Dakshina Kannada, Karnataka, India 574211</span>
               </li>
             </ul>
+            <p className="text-sm text-gray-300 text-center mt-6">&copy; {new Date().getFullYear()} Border Security System.</p>
+          </div>
+          <div>
           </div>
         </div>
       </div>
